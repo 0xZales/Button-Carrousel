f9f71c240e5febd8ddc99dc1a6a2b303bea59dbc
0xZales <rozalesamiral@gmail.com>
2022-12-31 22:43:51 +0000

    SImple carousel system

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import arrow from './assets/next.svg'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [width, setWidth] = useState(0);
+  const [scrollTime, setScrollTime] = useState(1);
+  const [btnWitdth, setBtbWidth] = useState(0);
+  const scroll = () => {
+    const container = document.querySelector("#carousel");
+    if (scrollTime !== 4) {
+      let size = window.screen.width;
+      container.style.transform = `translateX(${-size * scrollTime}px)`;
+      setScrollTime((l) => l + 1);
+    } else {
+      container.style.transform = `translateX(${0}px)`;
+      setScrollTime(1);
+    }
+  };
+  window.addEventListener("resize", (ev) => {
+    setWidth(window.screen.width *4);
+  });
+  useEffect(() => {
+    setWidth(window.screen.width*4);
+  },[]);
   return (
-    <div className="w-full h-full flex items-center justify-center flex-col gap-5">
-      <h1 className='text-[35px] font-bold'>Hey 0xZales let's build some great things</h1>
-  
-  
+    <div className={`relative w-[${width/4}]`}>
+     <img src={arrow} alt="Next Icon" className="md:hidden sm:hidden absolute top-[30%] right-0 z-10 w-[50px] " onClick={scroll} />
+      <div
+        className={`md:hidden sm:hidden  flex items-center  justify-start h-[150px] transition-all duration-700`} style={{width:width}}
+        id="carousel"
+      >
+        <button
+          className={`bg-blue-500 hover:bg-blue-700 text-white font-bold  w-[20%]  h-[60px] rounded-md m-[3%]`}
+          id="btn"
+        >
+          Bouton 1
+        </button>
+        <button
+          className={`bg-blue-500 hover:bg-blue-700 text-white font-bold  w-[20%]  h-[60px] rounded-md m-[3%]`}
+          id="btn"
+        >
+          Bouton 2
+        </button>
+        <button
+          className={`bg-blue-500 hover:bg-blue-700 text-white font-bold  w-[20%]  h-[60px] rounded-md m-[3%]`}
+          id="btn"
+        >
+          Bouton 3
+        </button>
+        <button
+          className={`bg-blue-500 hover:bg-blue-700 text-white font-bold  w-[20%]  h-[60px] rounded-md m-[3%]`}
+          id="btn"
+        >
+          Bouton 4
+        </button>
+      </div>
+     
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
